2071f24e4f53f6cd693e0753f9988c0b0216456c
Joeri Simons <joeri.simons@student.kuleuven.be>
2023-04-17 17:01:38 +0200

    add expense activity added

--- app/src/main/java/com/example/budgetbuddy/backendLogic/Expense.java
+++ app/src/main/java/com/example/budgetbuddy/backendLogic/Expense.java
@@ -1,4 +1,14 @@
 package com.example.budgetbuddy.backendLogic;
 
+import org.json.JSONObject;
+
 public class Expense {
+    private float amount;
+    private String date;
+    private String place;
+    private String description;
+    private String category;
+    public Expense(JSONObject o){
+
+    }
 }
