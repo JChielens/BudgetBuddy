06a62360e77aca05e001be8a9813b581257f48ed
Jonathan Chielens <jonathan.chielens@student.kuleuven.be>
2023-05-04 19:49:00 +0200

    Login activity added

---
+++ app/src/main/java/com/example/budgetbuddy/RemoveExpensesActivity.java
@@ -0,0 +1,14 @@
+package com.example.budgetbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RemoveExpensesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remove_expenses);
+    }
+}
