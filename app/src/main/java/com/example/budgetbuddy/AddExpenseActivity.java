9f229229750f788bba35902394f5177041a24d13
Jonathan Chielens <jonathan.chielens@student.kuleuven.be>
2023-05-22 17:58:07 +0200

    AddExpenseActivity: UI upgrades + euro prefix added + EditTexts changed to TextFields

--- app/src/main/java/com/example/budgetbuddy/AddExpenseActivity.java
+++ app/src/main/java/com/example/budgetbuddy/AddExpenseActivity.java
@@ -39,7 +39,6 @@ public class AddExpenseActivity extends AppCompatActivity {
     private EditText txtDescription;
     private EditText txtAmount;
     private Spinner categorySpinner;
-    private TextView txtDate;
     private EditText txtPlace;
     private TextView lblSelectedDate;
     private ArrayList<Expense> expenses;
@@ -51,11 +50,10 @@ public class AddExpenseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expense);
 
-        txtDescription = findViewById(R.id.txtDescription);
-        txtAmount = findViewById(R.id.txtAmount);
+        txtDescription = findViewById(R.id.inputTxtDescription);
+        txtAmount = findViewById(R.id.inputTxtAmount);
         categorySpinner = findViewById(R.id.spinner);
-        txtPlace = findViewById(R.id.txtPlace);
-        txtDate = findViewById(R.id.lblSelectedDate);
+        txtPlace = findViewById(R.id.inputTxtPlace);
         lblSelectedDate = findViewById(R.id.lblSelectedDate);
 
         Intent intent = getIntent();
